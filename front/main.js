e07f55fb59a891216cd8345bb57b02af6e689d62
Hiroki.T <hirodora@me.com>
2022-06-30 01:12:06 +0900

    add: メンテ

--- front/main.js
+++ front/main.js
@@ -1,9 +1,27 @@
 // set sfc status
-let sfcStatusList = ["B1", "B2", "B3", "B4", "M1", "M2", "D1", "D2", "21年入学"];
-let sfcStatusElement = document.getElementById("sfcStatus");
-const nowYear = (new Date).getFullYear();
-let statusIndex = sfcStatusList.length-1
-if ((nowYear - 2021) < sfcStatusList.length) {
-    statusIndex = nowYear - 2021;
+
+
+const sfcStatusList = ["B1", "B2", "B3", "B4", "M1", "M2", "D1", "D2", "2021年入学"];
+
+function getStatus() {
+    let now = new Date;
+    let enterYear = 2021;
+    let enterMonth = 4;
+    const nowYear = now.getFullYear();
+    const nowMonth = now.getMonth();
+
+    let idx;
+    if (nowMonth < enterMonth) {
+        idx = nowYear-1 - 2021;
+    } else {
+        idx = nowYear - 2021;
+    }
+    if (idx >= sfcStatusList.length) {
+        return sfcStatusList[sfcStatusList.length-1];
+    }
+    return sfcStatusList[idx];
 }
-sfcStatusElement.innerText = sfcStatusList[statusIndex];
+
+
+let sfcStatusElement = document.getElementById("sfcStatus");
+sfcStatusElement.innerText = getStatus();
